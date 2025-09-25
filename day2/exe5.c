ca95833f5c97381a523614654af6e1c810f809be
AyoubKhaoua <mr.ayoubkaoua@gmail.com>
2025-09-25 17:46:28 +0200

    change exe5

--- day2/exe5.c
+++ day2/exe5.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 int main (){
-    int nbr;
+    int nbr,note;
     printf("donner  un nombre entre supirieur a 0 at infirieur a 100 : ");
     scanf("%d",&nbr);
-    
+    note=nbr/10;
     switch(nbr){
-        case nbr>90:
-                    printf("A");
+        case 9:
+                    printf("ANote littérale : A");
+        case 8:
+                    printf("ANote littérale : B"); 
+        case 7:
+                    printf("ANote littérale : C"); 
+        case 6:
+                    printf("ANote littérale : D");
+        case 5:
+                    printf("ANote littérale : F");            
+                                                      
+        
+
+
     }
     return 0;
 }
